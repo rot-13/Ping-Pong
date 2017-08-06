db4c632cad38e54f1c07fb55e78a17911ab921f7
Gilad Goldberg <ggoldberg@paypal.com>
2017-08-06 13:26:14 +0300

    knex migration on deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/rot-13/Ping-Pong.git',
       path : '/home/pi/Ping-Pong/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && ./node_modules/knex/lib/bin/cli.js migrate:latest && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,10 @@ module.exports = {
     client: config.development.database.client,
     connection: config.development.database.connection,
     migrations: config.development.database.migrations
-  }
+  },
+  production: {
+    client: config.production.database.client,
+    connection: config.production.database.connection,
+    migrations: config.production.database.migrations
+  }, 
 };
